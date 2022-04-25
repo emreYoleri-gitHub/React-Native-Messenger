23741d13734d541eae96f0a6e27a06f7dd7bceca
Emre Yoleri <emreyoleri01@gmail.com>
2022-04-25 22:10:40 +0300

    Install Dependencies and Libraries

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+const API_KEY = "3ejn8e5j79n9";
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
