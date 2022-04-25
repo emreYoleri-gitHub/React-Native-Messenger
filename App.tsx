343b0ba722a4f0dbff9a11a0c1649355561dd54b
Emre Yoleri <emreyoleri01@gmail.com>
2022-04-25 22:17:06 +0300

    connect to user

--- App.tsx
+++ App.tsx
@@ -1,15 +1,32 @@
+import React, { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StreamChat } from "stream-chat";
 
 import useCachedResources from "./hooks/useCachedResources";
 import useColorScheme from "./hooks/useColorScheme";
 import Navigation from "./navigation";
 
 const API_KEY = "3ejn8e5j79n9";
+const client = StreamChat.getInstance(API_KEY);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
+  useEffect(() => {
+    const connectUser = async () => {
+      await client.connectUser(
+        {
+          id: "vadim",
+          name: "Cadim Savin",
+          image: "https://i.imgur.com/fR9Jz14.png",
+        },
+        client.devToken("vadim")
+      );
+    };
+
+    connectUser();
+  }, []);
 
   if (!isLoadingComplete) {
     return null;
